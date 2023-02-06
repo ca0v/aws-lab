2f82a01c54a3ced9295a6c1f157ca7d150dd4876
Corey Alix <coreyalix@gmail.com>
2023-02-06 14:49:57 -0500

    Tidy key rotation script

--- rotate-access-keys.js
+++ rotate-access-keys.js
@@ -1,28 +1,31 @@
-// import child_process from "child_process"
-child_process = require("child_process")
-
-// this utility will rotate the access keys for a given user
-// this means it will create a new access key and then delete the old one
-// if there are already two access keys, it will delete the oldest one 1st
-// If it deletes the access key that is currently being used, it will
-// update the credentials file with the newer access key before deleting
-
+// Rotate the access keys for a given user
+//
+// PRE: There may be 0-2 access keys for the given user
+// If no access keys exist, create one.
+// If one access key exists, create a new one before deleting the old one.
+// If two access keys exist, delete one, create one, delete one.
+//
+// POST: There will only be one access key once complete
+//
+// Requirements:
 // to run this utility, you need to have the aws cli installed
 // https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html
 
+child_process = require("child_process")
+
 function main() {
   // get the user name from the command line
-  var userName = process.argv[2]
+  const userName = process.argv[2]
   if (!userName) throw new Error("ERROR: no user name provided")
 
   // get the access keys for the given user
-  var accessKeys = getAccessKeys(userName)
+  let accessKeys = getAccessKeys(userName)
 
   // if there are two access keys, delete the oldest one
   if (accessKeys.length == 2) {
     // get the newest access key
-    var keepThisKey = accessKeys[0]
-    var deleteThisKey = accessKeys[1]
+    let keepThisKey = accessKeys[0]
+    let deleteThisKey = accessKeys[1]
     if (accessKeys[0].CreateDate < accessKeys[1].CreateDate) {
       keepThisKey = accessKeys[1]
       deleteThisKey = accessKeys[0]
@@ -31,35 +34,37 @@ function main() {
     // if the newest is Inactive, delete that one instead
     if (keepThisKey.Status == "Inactive") {
       // swap the access keys
-      var temp = keepThisKey
+      const temp = keepThisKey
       keepThisKey = deleteThisKey
       deleteThisKey = temp
     }
 
     // update the credentials file with the newest access key
-    updateCredentialsFile(userName, keepThisKey)
+    updateCredentialsFile(keepThisKey)
 
     // delete the oldest access key
-    deleteAccessKey(userName, deleteThisKey)
+    deleteAccessKey(deleteThisKey)
     accessKeys = getAccessKeys(userName)
   }
 
   if (accessKeys.length == 1) {
-    var deleteThisKey = accessKeys[0]
+    const deleteThisKey = accessKeys[0]
     // create a new access key
-    var newAccessKey = createAccessKey(userName)
+    const newAccessKey = createAccessKey(userName)
 
     // update the credentials file with the new access key
-    updateCredentialsFile(userName, newAccessKey)
+    updateCredentialsFile(newAccessKey)
 
     // delete the old access key
-    deleteAccessKey(userName, deleteThisKey)
+    deleteAccessKey(deleteThisKey)
     accessKeys = getAccessKeys(userName)
   }
 
-  // if there are no access keys, create one (but how?!)
+  // if there are no access keys, create one
   if (accessKeys.length == 0) {
-    var newAccessKey = createAccessKey(userName)
+    const newAccessKey = createAccessKey(userName)
+    // update the credentials file with the new access key
+    updateCredentialsFile(userName, newAccessKey)
   }
 
   // assert there is exactly one access key now
@@ -76,24 +81,27 @@ function getAccessKeys(userName) {
   return accessKeys
 }
 
-function deleteAccessKey(userName, accessKeyId) {
-  const cmd = `aws iam delete-access-key --user-name ${userName} --access-key-id ${accessKeyId.AccessKeyId}`
+function deleteAccessKey(accessKey) {
+  const userName = accessKey.UserName
+  const cmd = `aws iam delete-access-key --user-name ${userName} --access-key-id ${accessKey.AccessKeyId}`
   runCommand(cmd)
 }
 
 function createAccessKey(userName) {
-  var cmd = "aws iam create-access-key --user-name " + userName
-  var result = runCommand(cmd)
-  var accessKey = JSON.parse(result).AccessKey
-  return accessKey
+  const cmd = `aws iam create-access-key --user-name ${userName}`
+  const result = runCommand(cmd)
+  return JSON.parse(result).AccessKey
 }
 
-function updateCredentialsFile(userName, accessKey) {
-  var cmd = `aws --profile ${userName} configure set aws_access_key_id ${accessKey.AccessKeyId}`
-  runCommand(cmd)
+function updateCredentialsFile(accessKey) {
+  const userName = accessKey.UserName
+  runCommand(
+    `aws --profile ${userName} configure set aws_access_key_id ${accessKey.AccessKeyId}`
+  )
   if (accessKey.SecretAccessKey) {
-    cmd = `aws  --profile ${userName} configure set aws_secret_access_key ${accessKey.SecretAccessKey}`
-    runCommand(cmd)
+    runCommand(
+      `aws  --profile ${userName} configure set aws_secret_access_key ${accessKey.SecretAccessKey}`
+    )
   }
 }
 
